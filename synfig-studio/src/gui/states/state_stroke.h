8eedb62cf5b0be0354e9fc4070fe5be2da2cc7c7
unknown <dhairyabahl5@gmail.com>
2021-05-19 14:57:29 +0530

    experimentation

--- synfig-studio/src/gui/states/state_stroke.h
+++ synfig-studio/src/gui/states/state_stroke.h
@@ -27,7 +27,7 @@
 
 /* === H E A D E R S ======================================================= */
 
-#include <ETL/smart_ptr.h>
+#include <ETL/_smart_ptr.h>
 #include <gui/canvasview.h>
 #include <gui/smach.h>
 #include <list>
